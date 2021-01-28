2f5df0fcc80fae73c18c386bac1a87fbd2a1425c
Wisma Eka <wismaeka@Wismas-MacBook-Pro.local>
2021-01-28 20:57:39 +0700

    added new ways

--- first/main.go
+++ first/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Println("Hi, you can call me", os.Args[1])
+
+	fmt.Print("Enter your name: \n")
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("hi, your name is:", input)
+
 }
